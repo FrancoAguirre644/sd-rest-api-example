bdbff3e8283790d9b0d39264183339f6df6e2d50
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-29 00:56:13 -0300

    add vehicle update endpoint

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -2,10 +2,13 @@ import {
     Body,
     Controller,
     Get,
+    Param,
+    Patch,
     Post,
 } from '@nestjs/common';
 
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
 import { VehiclesService } from './vehicles.service';
 
 @Controller('vehicles')
@@ -19,8 +22,21 @@ export class VehiclesController {
         return this.vehiclesService.findAll();
     }
 
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.vehiclesService.findOne(Number(id));
+    }
+
     @Post()
     create(@Body() createVehicleDto: CreateVehicleDto) {
         return this.vehiclesService.create(createVehicleDto);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateVehicleDto: UpdateVehicleDto) {
+        return this.vehiclesService.update(
+            Number(id),
+            updateVehicleDto,
+        );
     }
 }
--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
 import { VehicleEntity } from './entities/vehicle.entity';
 
 @Injectable()
@@ -10,10 +11,18 @@ export class VehiclesService {
   constructor(
     @InjectRepository(VehicleEntity)
     private readonly vehicleRepository: Repository<VehicleEntity>,
-  ) { }
+  ) {}
 
   async findAll(): Promise<VehicleEntity[]> {
     return this.vehicleRepository.find();
+  }
+
+  async findOne(id: number): Promise<VehicleEntity | null> {
+    return this.vehicleRepository.findOne({
+      where: {
+        id,
+      },
+    });
   }
 
   async create(
@@ -23,6 +32,25 @@ export class VehiclesService {
       ...createVehicleDto,
       active: true,
     });
+
+    return this.vehicleRepository.save(vehicle);
+  }
+
+  async update(
+    id: number,
+    updateVehicleDto: UpdateVehicleDto,
+  ): Promise<VehicleEntity | null> {
+    const vehicle = await this.vehicleRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!vehicle) {
+      return null;
+    }
+
+    Object.assign(vehicle, updateVehicleDto);
 
     return this.vehicleRepository.save(vehicle);
   }
