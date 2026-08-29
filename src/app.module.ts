8029159439c3d185494f5dfdf8d6369a5ccc590c
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-29 01:18:35 -0300

    add .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { VehiclesModule } from './vehicles/vehicles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'vehicles',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
     VehiclesModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
