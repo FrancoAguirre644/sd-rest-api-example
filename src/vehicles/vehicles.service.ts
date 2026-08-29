a47795b2cd8d9d3c0fdb2252cb87cdd655f1f174
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-29 00:45:27 -0300

    add vehicle creation service

--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { VehicleEntity } from './entities/vehicle.entity';
 
 @Injectable()
@@ -10,4 +11,15 @@ export class VehiclesService {
     @InjectRepository(VehicleEntity)
     private readonly vehicleRepository: Repository<VehicleEntity>,
   ) {}
+
+  async create(
+    createVehicleDto: CreateVehicleDto,
+  ): Promise<VehicleEntity> {
+    const vehicle = this.vehicleRepository.create({
+      ...createVehicleDto,
+      active: true,
+    });
+
+    return this.vehicleRepository.save(vehicle);
+  }
 }
