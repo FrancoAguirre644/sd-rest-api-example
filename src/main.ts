c37eb7400859d44b23179c0bc580d332c73afcc4
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-29 01:10:42 -0300

    docs: configure Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Vehicles API')
+    .setDescription('REST API for vehicle management')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
